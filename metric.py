9e57f2001571a4bdb2f2c44d89434a52a2da16ce
Manoj Kumar Singh <manojsinghabc@gmail.com>
2020-10-15 23:59:56 +0530

    Update metric.py

--- metric.py
+++ metric.py
@@ -1,21 +1,21 @@
 import numpy as np
-def eval_metric(labels, preds, log_sold_price_groups = [0,50,100, 500, 1000] ):
-    labels = np.array(labels)
-    preds = np.array(preds)
+
+def eval_metric_bucket(labels, preds, log_sold_price_groups = [0,50,100, 500, 1000]):
+    labels = np.array(labels).flatten()
+    preds = np.array(preds).flatten()
+    SE = (labels - preds)**2
     metrics = []
     for i in range(len(log_sold_price_groups)):
         try:
-            Filter = (labels >= log_sold_price_groups[i]) & (labels < log_sold_price_groups[i+1])
-            print(Filter)
+            mask_ = (labels >= log_sold_price_groups[i]) & (labels < log_sold_price_groups[i+1])
             try:
-                metrics.append(mean_squared_error(labels[Filter], preds[Filter]))
+                metrics.append(np.sqrt(SE[mask_].mean()))
             except:
                 metrics.append(np.nan)
         except:
-            Filter = (labels >= log_sold_price_groups[i])
-            print(Filter)
+            mask_ = (labels >= log_sold_price_groups[i])
             try:
-                metrics.append(mean_squared_error(labels[Filter], preds[Filter]))
+                metrics.append(np.sqrt(SE[mask_].mean()))
             except:
                 metrics.append(np.nan)
     
