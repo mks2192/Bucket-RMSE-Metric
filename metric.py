8a3bc61c4ea5fa884a738a43f385b7bc9aec25fd
mks2192 <manojsinghabc@gmail.com>
2020-11-26 22:54:12 +0530

    changes

--- metric.py
+++ metric.py
@@ -1,23 +1,24 @@
 import numpy as np
-
-def eval_metric_bucket(labels, preds, log_sold_price_groups = [0,50,100, 500, 1000]):
+from sklearn.metrics import mean_squared_error
+def eval_metric(labels, preds, log_sold_price_groups = [0,50,100, 500, 1000] ):
     labels = np.array(labels).flatten()
     preds = np.array(preds).flatten()
-    SE = (labels - preds)**2
     metrics = []
     for i in range(len(log_sold_price_groups)):
         try:
-            mask_ = (labels >= log_sold_price_groups[i]) & (labels < log_sold_price_groups[i+1])
+            Filter = (labels >= log_sold_price_groups[i]) & (labels < log_sold_price_groups[i+1])
+            print(Filter)
             try:
-                metrics.append(np.sqrt(SE[mask_].mean()))
+                metrics.append(mean_squared_error(labels[Filter], preds[Filter]))
             except:
                 metrics.append(np.nan)
         except:
-            mask_ = (labels >= log_sold_price_groups[i])
+            Filter = (labels >= log_sold_price_groups[i])
+            print(Filter)
             try:
-                metrics.append(np.sqrt(SE[mask_].mean()))
+                metrics.append(mean_squared_error(labels[Filter], preds[Filter]))
             except:
                 metrics.append(np.nan)
     
     return np.array(metrics)
-    #
+    ##
