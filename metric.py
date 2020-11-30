b5d7679c2c579637a217b1629aea31e7a1b2eceb
mks2192 <manojsinghabc@gmail.com>
2020-12-01 03:46:02 +0530

    changes

--- metric.py
+++ metric.py
@@ -1,19 +1,19 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
-def eval_metric(labels, preds, log_sold_price_groups = [0,50,100, 500, 1000] ):
+def eval_metric(labels, preds, bucket = [0,50,100, 500, 1000] ):
     labels = np.array(labels).flatten()
     preds = np.array(preds).flatten()
     metrics = []
-    for i in range(len(log_sold_price_groups)):
+    for i in range(len(bucket)):
         try:
-            mask_ = (labels >= log_sold_price_groups[i]) & (labels < log_sold_price_groups[i+1])
+            mask_ = (labels >= bucket[i]) & (labels < bucket[i+1])
             print(mask_)
             try:
                 metrics.append(mean_squared_error(labels[mask_], preds[mask_]))
             except:
                 metrics.append(np.nan)
         except:
-            mask_ = (labels >= log_sold_price_groups[i])
+            mask_ = (labels >= bucket[i])
             print(mask_)
             try:
                 metrics.append(mean_squared_error(labels[mask_], preds[mask_]))
